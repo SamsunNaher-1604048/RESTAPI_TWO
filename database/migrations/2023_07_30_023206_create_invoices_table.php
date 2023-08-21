77390f4db0850369a06eadddd9f3522f2a6bf4b1
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2023-08-21 08:11:32 +0600

    day_3

--- database/migrations/2023_07_30_023206_create_invoices_table.php
+++ database/migrations/2023_07_30_023206_create_invoices_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->integer('customer_id');
             $table->integer('amount');
             $table->string('status');
-            $table->dateTime('billrd_dated');
+            $table->dateTime('billed_date');
             $table->dateTime('paid_date')->nullable();
             $table->timestamps();
         });
